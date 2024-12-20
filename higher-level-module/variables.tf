c110e95137f6a1b0f5a61e0bbf0e81892312a330
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 09:33:10 +0530

    Update variables.tf

--- higher-level-module/variables.tf
+++ higher-level-module/variables.tf
@@ -1,1 +1,41 @@
+# VPC Module Inputs
+variable "vpc_cidr_block" {
+  type = string
+}
 
+# EC2 Module Inputs
+variable "ami_id" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+variable "ec2_subnet_id" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
+
+# RDS Module Inputs
+variable "rds_allocated_storage" {
+  type = number
+}
+variable "rds_engine" {
+  type = string
+}
+variable "rds_instance_class" {
+  type = string
+}
+variable "rds_db_name" {
+  type = string
+}
+variable "rds_username" {
+  type = string
+}
+variable "rds_password" {
+  type = string
+}
+variable "rds_db_subnet_group" {
+  type = string
+}
