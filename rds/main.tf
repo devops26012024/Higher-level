3fc3b769733bbab53191107a28cafbcd8af19c81
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 09:31:32 +0530

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -1,1 +1,13 @@
+resource "aws_db_instance" "db" {
+  allocated_storage    = var.allocated_storage
+  engine               = var.engine
+  instance_class       = var.instance_class
+  name                 = var.db_name
+  username             = var.username
+  password             = var.password
+  subnet_group_name    = var.db_subnet_group
+}
 
+output "db_endpoint" {
+  value = aws_db_instance.db.endpoint
+}
