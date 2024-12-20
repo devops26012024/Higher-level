68a68b99926f27fa4505ac69999f55c00b1073cf
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 10:51:37 +0530

    Update main.tf

--- higher-level-module/main.tf
+++ higher-level-module/main.tf
@@ -1,23 +1,23 @@
 module "vpc" {
   source      = "../VPC"
-  #cidr_block  = var.vpc_cidr_block
+  cidr_block  = var.vpc_cidr_block
 }
 
 module "ec2" {
   source        = "../ec2"
-  #ami_id        = var.ami_id
-  #instance_type = var.instance_type
-  #subnet_id     = var.ec2_subnet_id
-  #key_name      = var.key_name
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.ec2_subnet_id
+  key_name      = var.key_name
 }
 
 module "rds" {
   source             = "../rds"
-  #allocated_storage  = var.rds_allocated_storage
-  #engine             = var.rds_engine
-  #instance_class     = var.rds_instance_class
-  #db_name            = var.rds_db_name
-  #username           = var.rds_username
-  #password           = var.rds_password
-  #db_subnet_group    = var.rds_db_subnet_group
+  allocated_storage  = var.rds_allocated_storage
+  engine             = var.rds_engine
+  instance_class     = var.rds_instance_class
+  db_name            = var.rds_db_name
+  username           = var.rds_username
+  password           = var.rds_password
+  db_subnet_group    = var.rds_db_subnet_group
 }
