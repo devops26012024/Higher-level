12d2b3b0c30e95c8d07a0fea228621872201679c
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 09:34:52 +0530

    Create Root_Main.tf

---
+++ Root_Main.tf
@@ -0,0 +1,16 @@
+module "infra" {
+  source = "./terraform-modules/higher-level"
+
+  vpc_cidr_block       = "10.0.0.0/16"
+  ami_id               = "ami-12345678"
+  instance_type        = "t2.micro"
+  ec2_subnet_id        = "subnet-abc123"
+  key_name             = "my-key-pair"
+  rds_allocated_storage = 20
+  rds_engine           = "mysql"
+  rds_instance_class   = "db.t2.micro"
+  rds_db_name          = "mydatabase"
+  rds_username         = "admin"
+  rds_password         = "password123"
+  rds_db_subnet_group  = "my-subnet-group"
+}
