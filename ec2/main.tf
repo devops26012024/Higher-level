054064bc6f55944cac4751c5b1f1ef60eb424acb
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 09:30:44 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,10 @@
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+}
 
+output "instance_id" {
+  value = aws_instance.app.id
+}
