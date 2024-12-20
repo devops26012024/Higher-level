2d3df791790009694f72442573662f4a013a8108
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 10:59:52 +0530

    Update variables.tf

--- rds/variables.tf
+++ rds/variables.tf
@@ -1,1 +1,27 @@
+variable "allocated_storage" {
+  type = number
+}
 
+variable "engine" {
+  type = string
+}
+
+variable "instance_class" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "db_subnet_group" {
+  type = string
+}
