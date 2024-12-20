e48d1148d1a19fb61289d7c221c76f8b8d0bd209
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 11:25:54 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+#  subnet_id     = var.subnet_id
   key_name      = var.key_name
 }
 
